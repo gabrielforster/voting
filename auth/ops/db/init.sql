d1ea16c73ae4192c12a30a3e4017c7d6dc761b24
Gabriel Rocha <rochafrgabriel@gmail.com>
2024-04-19 23:36:26 -0300

    fix: change table name to plural

--- auth/ops/db/init.sql
+++ auth/ops/db/init.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS auth_db;
 
 USE auth_db;
 
-CREATE TABLE IF NOT EXISTS user (
+CREATE TABLE IF NOT EXISTS `users` (
     id INT PRIMARY KEY,
     email VARCHAR(255),
     password VARCHAR(255),
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS user (
 ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 
-INSERT INTO user (id, email, password, first_name, last_name, created_at, updated_at)
+INSERT INTO `users` (id, email, password, first_name, last_name, created_at, updated_at)
 SELECT 1,'rochafrgabriel@gmail.com', SHA1('1234'), 'Gabriel', 'Rocha', now(), null FROM DUAL
 WHERE NOT EXISTS
-    (SELECT email FROM user WHERE email='rochafrgabriel@gmail.com');
+    (SELECT email FROM `users` WHERE email='rochafrgabriel@gmail.com');
